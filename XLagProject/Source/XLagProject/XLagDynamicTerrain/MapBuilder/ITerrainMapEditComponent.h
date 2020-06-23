11050b46ad8ec471bfe8b807bc314cde165d7dc5
Ганюшин Роман <r.ganyushin@lan-project.ru>
2020-06-24 00:53:28 +0300

    Начал делать операции с землей

--- XLagProject/Source/XLagProject/XLagDynamicTerrain/MapBuilder/ITerrainMapEditComponent.h
+++ XLagProject/Source/XLagProject/XLagDynamicTerrain/MapBuilder/ITerrainMapEditComponent.h
@@ -4,5 +4,6 @@
 struct ITerrainMapEditComponent
 {
 	virtual ~ITerrainMapEditComponent() = default;
+	virtual void PrepareForEdit(ITerrainMapAccessor *const accessor) {};
 	virtual void DoEdit(ITerrainMapAccessor *const accessor, const int& ix, const int& iy) = 0;
 };
